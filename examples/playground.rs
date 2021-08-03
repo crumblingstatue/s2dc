7649e599f1f8d7c351abfcb8326d782b87802edc
crumblingstatue <radiantstatue@gmail.com>
2021-08-03 21:42:18 +0200

    Madness

--- examples/playground.rs
+++ examples/playground.rs
@@ -1,35 +1,109 @@
 use macroquad::prelude::*;
 use s2dc::Rect;
 
+enum Control {
+    Orig,
+    Proj,
+}
+
+impl Control {
+    fn name(&self) -> &str {
+        match *self {
+            Self::Orig => "original",
+            Self::Proj => "projected",
+        }
+    }
+}
+
 #[macroquad::main("s2dc playground")]
 async fn main() {
-    let mut player_rect = Rect::new(100., 100., 32., 32.);
-    let solids = [Rect::new(200., 200., 64., 64.)];
+    let mut orig_rect = Rect::new(100., 100., 32., 32.).unwrap();
+    let mut proj_rect = orig_rect;
+    let mut ctrl = Control::Orig;
+    let mut solids = Vec::new();
+    let mut start_point = None;
     loop {
-        let speed = 7.3;
+        let speed = if is_key_down(KeyCode::LeftShift) {
+            1.0
+        } else {
+            8.0
+        };
+        let controlled = match ctrl {
+            Control::Orig => &mut orig_rect,
+            Control::Proj => &mut proj_rect,
+        };
+        if is_key_pressed(KeyCode::Enter) {
+            ctrl = match ctrl {
+                Control::Orig => Control::Proj,
+                Control::Proj => Control::Orig,
+            };
+        }
         if is_key_down(KeyCode::Left) {
-            player_rect.x -= speed;
+            *controlled.x_mut() -= speed;
         } else if is_key_down(KeyCode::Right) {
-            player_rect.x += speed;
+            *controlled.x_mut() += speed;
         }
         if is_key_down(KeyCode::Up) {
-            player_rect.y -= speed;
+            *controlled.y_mut() -= speed;
         } else if is_key_down(KeyCode::Down) {
-            player_rect.y += speed;
+            *controlled.y_mut() += speed;
         }
-        draw_rectangle(
-            player_rect.x,
-            player_rect.y,
-            player_rect.w,
-            player_rect.h,
-            WHITE,
-        );
-        for solid in &solids {
-            draw_rectangle(solid.x, solid.y, solid.w, solid.h, BROWN);
-            if let Some(intersect) = player_rect.intersection(solid) {
-                draw_rectangle(intersect.x, intersect.y, intersect.w, intersect.h, YELLOW);
+        if is_mouse_button_pressed(MouseButton::Left) {
+            let mp = mouse_position();
+            start_point = Some(mp);
+        }
+        if let Some(point) = start_point {
+            let mp = mouse_position();
+            if let Some(rect) = Rect::new(point.0, point.1, mp.0 - point.0, mp.1 - point.1) {
+                draw_rectangle_lines(*rect.x(), *rect.y(), *rect.w(), *rect.h(), 1.0, RED);
+                if is_mouse_button_released(MouseButton::Left) {
+                    solids.push(rect);
+                    start_point = None;
+                }
             }
         }
+        let ctrl_rect = *controlled;
+        draw_text(
+            &format!("Controlling {} (enter)", ctrl.name()),
+            0.,
+            32.,
+            32.,
+            WHITE,
+        );
+        draw_rectangle(
+            *orig_rect.x(),
+            *orig_rect.y(),
+            *orig_rect.w(),
+            *orig_rect.h(),
+            WHITE,
+        );
+        draw_rectangle(
+            *proj_rect.x(),
+            *proj_rect.y(),
+            *proj_rect.w(),
+            *proj_rect.h(),
+            YELLOW,
+        );
+        let new = s2dc::solve(&orig_rect, &proj_rect, &solids);
+        for solid in &solids {
+            draw_rectangle(*solid.x(), *solid.y(), *solid.w(), *solid.h(), BROWN);
+            if let Some(intersect) = proj_rect.intersection(solid) {
+                draw_rectangle(
+                    *intersect.x(),
+                    *intersect.y(),
+                    *intersect.w(),
+                    *intersect.h(),
+                    RED,
+                );
+            }
+        }
+        let new_x = *new.x();
+        let new_y = *new.y();
+        draw_line(new_x + 16., new_y - 16., new_x + 16., new_y, 2.0, GREEN);
+        draw_line(new_x + 8., new_y - 8., new_x + 16., new_y, 2.0, GREEN);
+        draw_line(new_x + 24., new_y - 8., new_x + 16., new_y, 2.0, GREEN);
+        draw_rectangle(new_x, new_y, *new.w(), *new.h(), GREEN);
+        draw_text("C", *ctrl_rect.x() + 8.0, *ctrl_rect.y() + 24.0, 32.0, BLUE);
         next_frame().await
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,41 @@
-use std::ops::{Add, Sub};
+use num_traits::Num;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct Rect<T> {
-    pub x: T,
-    pub y: T,
-    pub w: T,
-    pub h: T,
+    x: T,
+    y: T,
+    w: T,
+    h: T,
 }
 
 impl<T> Rect<T> {
-    pub fn new(x: T, y: T, w: T, h: T) -> Self {
-        Self { x, y, w, h }
+    pub fn x(&self) -> &T {
+        &self.x
+    }
+    pub fn x_mut(&mut self) -> &mut T {
+        &mut self.x
+    }
+    pub fn y(&self) -> &T {
+        &self.y
+    }
+    pub fn y_mut(&mut self) -> &mut T {
+        &mut self.y
+    }
+    pub fn w(&self) -> &T {
+        &self.w
+    }
+    pub fn h(&self) -> &T {
+        &self.h
+    }
+}
+
+impl<T: Num + PartialOrd> Rect<T> {
+    pub fn new(x: T, y: T, w: T, h: T) -> Option<Self> {
+        if w < T::zero() || h < T::zero() {
+            None
+        } else {
+            Some(Self { x, y, w, h })
+        }
     }
 }
 
@@ -22,7 +47,7 @@ fn minmax<T: PartialOrd>(a: T, b: T) -> (T, T) {
     }
 }
 
-impl<T: Add<T, Output = T> + Sub<T, Output = T> + Copy + PartialOrd> Rect<T> {
+impl<T: Num + PartialOrd + Copy> Rect<T> {
     pub fn intersection(&self, other: &Self) -> Option<Self> {
         let self_right = self.x + self.w;
         let other_right = other.x + other.w;
@@ -47,9 +72,9 @@ impl<T: Add<T, Output = T> + Sub<T, Output = T> + Copy + PartialOrd> Rect<T> {
 #[test]
 fn test_intersection() {
     use assert_matches::assert_matches;
-    let rect1 = Rect::new(12, 31, 20, 21);
-    let rect2 = Rect::new(19, 42, 37, 31);
-    let rect3 = Rect::new(41, 30, 34, 55);
+    let rect1 = Rect::new(12, 31, 20, 21).unwrap();
+    let rect2 = Rect::new(19, 42, 37, 31).unwrap();
+    let rect3 = Rect::new(41, 30, 34, 55).unwrap();
     assert_matches!(
         rect1.intersection(&rect2),
         Some(Rect {
@@ -88,4 +113,19 @@ fn test_intersection() {
     );
     assert_matches!(rect1.intersection(&rect3), None);
     assert_matches!(rect3.intersection(&rect1), None);
+}
+
+/// Takes in an original rectangle, the rectangle where the object should be (projected), and
+/// calculates the new position taking solid objects between these two rectangles into account.
+pub fn solve<T: Num + PartialOrd + Copy>(
+    original: &Rect<T>,
+    projected: &Rect<T>,
+    solids: &[Rect<T>],
+) -> Rect<T> {
+    for solid in solids {
+        if solid.intersection(projected).is_some() {
+            return *original;
+        }
+    }
+    *projected
 }
