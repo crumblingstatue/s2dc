7b2939d85cc3933830cdf2f6c1dbb4330106ef24
crumblingstatue <radiantstatue@gmail.com>
2021-08-05 19:23:22 +0200

    Finally working

--- examples/playground.rs
+++ examples/playground.rs
@@ -1,4 +1,4 @@
-use c::{collision, pos_and_bb_centered, pos_bb_xywh};
+use c::{collision, pos_and_bb_centered, pos_bb_xywh, Entity};
 use macroquad::prelude::*;
 use s2dc as c;
 
@@ -6,24 +6,26 @@ use s2dc as c;
 async fn main() {
     let mut solids = Vec::new();
     let mut start_point = None;
-    let player_bb = c::vec2(16, 16);
-    let mut player_pos = c::vec2(100, 100);
+    let mut player = c::Entity {
+        pos: c::vec2(100, 100),
+        bb: c::vec2(16, 16),
+    };
     loop {
         let speed = if is_key_down(KeyCode::LeftShift) {
-            1
+            1.
         } else {
-            8
+            17.62
         };
-        let controlled = &mut player_pos;
+        let mut accum = 0.0;
         if is_key_down(KeyCode::Left) {
-            controlled.x -= speed;
+            c::move_x(&mut player, -speed, &mut accum, &solids);
         } else if is_key_down(KeyCode::Right) {
-            controlled.x += speed;
+            c::move_x(&mut player, speed, &mut accum, &solids);
         }
         if is_key_down(KeyCode::Up) {
-            controlled.y -= speed;
+            c::move_y(&mut player, -speed, &mut accum, &solids);
         } else if is_key_down(KeyCode::Down) {
-            controlled.y += speed;
+            c::move_y(&mut player, speed, &mut accum, &solids);
         }
         if is_mouse_button_pressed(MouseButton::Left) {
             let mp = mouse_position();
@@ -36,7 +38,7 @@ async fn main() {
             let (x, y, w, h) = pos_bb_xywh(pos, bb);
             draw_rectangle_lines(x as f32, y as f32, w as f32, h as f32, 1.0, RED);
             if is_mouse_button_released(MouseButton::Left) {
-                solids.push((pos, bb));
+                solids.push(Entity { pos, bb });
                 start_point = None;
             }
             draw_rectangle(point.0, point.1, 1., 1., MAGENTA);
@@ -51,20 +53,20 @@ async fn main() {
             draw_rectangle(pos.x as f32, pos.y as f32, 1., 1., YELLOW);
         }
         let mut player_color = WHITE;
-        for &(pos, bb) in &solids {
-            let (x, y, w, h) = pos_bb_xywh(pos, bb);
+        for solid in &solids {
+            let (x, y, w, h) = pos_bb_xywh(solid.pos, solid.bb);
             let mut c2 = BROWN;
-            if collision(player_pos, player_bb, pos, bb) {
+            if collision(&player, solid, c::vec2(0, 0)) {
                 player_color = RED;
                 c2 = YELLOW;
             }
             draw_rectangle(x as f32, y as f32, w as f32, h as f32, c2);
         }
-        let (x, y, w, h) = pos_bb_xywh(player_pos, player_bb);
+        let (x, y, w, h) = pos_bb_xywh(player.pos, player.bb);
         draw_rectangle(x as f32, y as f32, w as f32, h as f32, player_color);
         draw_rectangle(
-            player_pos.x as f32 - 1.,
-            player_pos.y as f32 - 1.,
+            player.pos.x as f32 - 1.,
+            player.pos.y as f32 - 1.,
             2.,
             2.,
             MAGENTA,
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! Simple 2d solid collision library.
+//!
+//! Based on https://maddythorson.medium.com/celeste-and-towerfall-physics-d24bd2ae0fc5
+
 type Unit = i32;
 
 #[derive(Clone, Copy, Debug)]
@@ -30,9 +34,65 @@ pub fn pos_bb_xywh(pos: Vec2, bb: Vec2) -> (Unit, Unit, Unit, Unit) {
     (pos.x - bb.x, pos.y - bb.y, bb.x * 2, bb.y * 2)
 }
 
-pub fn collision(p1: Vec2, bb1: Vec2, p2: Vec2, bb2: Vec2) -> bool {
-    p1.x + bb1.x > p2.x - bb2.x
-        && p1.x - bb1.x < p2.x + bb2.x
-        && p1.y + bb1.y > p2.y - bb2.y
-        && p1.y - bb1.y < p2.y + bb2.y
+pub fn collision(en1: &Entity, en2: &Entity, en1_offset: Vec2) -> bool {
+    let x = en1.pos.x + en1_offset.x;
+    let y = en1.pos.y + en1_offset.y;
+    x + en1.bb.x > en2.pos.x - en2.bb.x
+        && x - en1.bb.x < en2.pos.x + en2.bb.x
+        && y + en1.bb.y > en2.pos.y - en2.bb.y
+        && y - en1.bb.y < en2.pos.y + en2.bb.y
+}
+
+pub struct Entity {
+    pub pos: Vec2,
+    pub bb: Vec2,
+}
+
+fn collide(entity: &Entity, solids: &[Entity], offset: Vec2) -> bool {
+    for s in solids {
+        if collision(entity, s, offset) {
+            return true;
+        }
+    }
+    false
+}
+
+pub fn move_x(entity: &mut Entity, amount: f32, accum: &mut f32, solids: &[Entity]) -> bool {
+    *accum += amount;
+    let mut move_amount = accum.round() as i32;
+    if move_amount != 0 {
+        *accum -= move_amount as f32;
+        let sign = move_amount.signum();
+        while move_amount != 0 {
+            if !collide(entity, solids, vec2(sign, 0)) {
+                //There is no Solid immediately beside us
+                entity.pos.x += sign;
+                move_amount -= sign;
+            } else {
+                //Hit a solid!
+                return true;
+            }
+        }
+    }
+    false
+}
+
+pub fn move_y(entity: &mut Entity, amount: f32, accum: &mut f32, solids: &[Entity]) -> bool {
+    *accum += amount;
+    let mut move_amount = accum.round() as i32;
+    if move_amount != 0 {
+        *accum -= move_amount as f32;
+        let sign = move_amount.signum();
+        while move_amount != 0 {
+            if !collide(entity, solids, vec2(0, sign)) {
+                //There is no Solid immediately beside us
+                entity.pos.y += sign;
+                move_amount -= sign;
+            } else {
+                //Hit a solid!
+                return true;
+            }
+        }
+    }
+    false
 }
