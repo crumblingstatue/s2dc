8fac97e9127912f0a660405ff6379e6c443148ad
crumblingstatue <radiantstatue@gmail.com>
2022-04-27 20:35:45 +0200

    Clean up library and add documentation

--- examples/playground.rs
+++ examples/playground.rs
@@ -1,4 +1,3 @@
-use c::{collision, pos_and_bb_centered, pos_bb_xywh, Entity};
 use macroquad::prelude::*;
 use s2dc as c;
 
@@ -6,47 +5,47 @@ use s2dc as c;
 async fn main() {
     let mut solids = Vec::new();
     let mut start_point = None;
-    let mut player = c::Entity {
-        pos: c::vec2(100, 100),
-        bb: c::vec2(16, 16),
-    };
+    let mut player = c::MobileEntity::from_pos_and_bb(c::vec2(100, 100), c::vec2(16, 16));
     let mut vspeed = 0.0;
     let gravity = 1.4;
     let mut platformer_mode = false;
+    let mut solid_collision = true;
     loop {
         if is_key_pressed(KeyCode::Tab) {
             platformer_mode = !platformer_mode;
+        }
+        if is_key_pressed(KeyCode::C) {
+            solid_collision ^= true;
         }
         let speed = if is_key_down(KeyCode::LeftShift) {
             1.
         } else {
             17.62
         };
-        let mut accum = 0.0;
-        let collider = |en: &Entity, offs| {
+        let collider = |en: &c::Entity, offs| {
             for solid in &solids {
-                if collision(en, solid, offs) {
+                if en.would_collide(solid, offs) {
                     return true;
                 }
             }
             false
         };
         if is_key_down(KeyCode::Left) {
-            c::move_x(&mut player, -speed, &mut accum, collider);
+            player.move_x(-speed, collider);
         } else if is_key_down(KeyCode::Right) {
-            c::move_x(&mut player, speed, &mut accum, collider);
+            player.move_x(speed, collider);
         }
         if is_key_down(KeyCode::Up) {
-            c::move_y(&mut player, -speed, &mut accum, collider);
+            player.move_y(-speed, collider);
         } else if is_key_down(KeyCode::Down) {
-            c::move_y(&mut player, speed, &mut accum, collider);
+            player.move_y(speed, collider);
         }
         if platformer_mode {
             vspeed += gravity;
             if is_key_pressed(KeyCode::Space) {
                 vspeed = -16.0;
             }
-            if c::move_y(&mut player, vspeed, &mut accum, collider) {
+            if player.move_y(vspeed, collider) {
                 vspeed = 0.0;
             }
         }
@@ -56,51 +55,60 @@ async fn main() {
         }
         if let Some(point) = start_point {
             let mp = mouse_position();
-            let (pos, bb) =
-                pos_and_bb_centered(point.0 as i32, point.1 as i32, mp.0 as i32, mp.1 as i32);
-            let (x, y, w, h) = pos_bb_xywh(pos, bb);
+            let en = c::Entity::from_rect_corners(
+                point.0 as i32,
+                point.1 as i32,
+                mp.0 as i32,
+                mp.1 as i32,
+            );
+            let (x, y, w, h) = en.xywh();
             draw_rectangle_lines(x as f32, y as f32, w as f32, h as f32, 1.0, RED);
             if is_mouse_button_released(MouseButton::Left) {
-                solids.push(Entity { pos, bb });
+                solids.push(en);
                 start_point = None;
             }
             draw_rectangle(point.0, point.1, 1., 1., MAGENTA);
             draw_line(
-                pos.x as f32,
-                pos.y as f32,
-                pos.x as f32 + bb.x as f32,
-                pos.y as f32 + bb.y as f32,
+                en.pos.x as f32,
+                en.pos.y as f32,
+                en.pos.x as f32 + en.bb.x as f32,
+                en.pos.y as f32 + en.bb.y as f32,
                 1.0,
                 GREEN,
             );
-            draw_rectangle(pos.x as f32, pos.y as f32, 1., 1., YELLOW);
+            draw_rectangle(en.pos.x as f32, en.pos.y as f32, 1., 1., YELLOW);
         }
         let mut player_color = WHITE;
         for solid in &solids {
-            let (x, y, w, h) = pos_bb_xywh(solid.pos, solid.bb);
+            let (x, y, w, h) = solid.xywh();
             let mut c2 = BROWN;
-            if collision(&player, solid, c::vec2(0, 0)) {
+            if player.en.would_collide(solid, c::vec2(0, 0)) {
                 player_color = RED;
                 c2 = YELLOW;
             }
             draw_rectangle(x as f32, y as f32, w as f32, h as f32, c2);
         }
-        let (x, y, w, h) = pos_bb_xywh(player.pos, player.bb);
+        let (x, y, w, h) = player.en.xywh();
         draw_rectangle(x as f32, y as f32, w as f32, h as f32, player_color);
         draw_rectangle(
-            player.pos.x as f32 - 1.,
-            player.pos.y as f32 - 1.,
+            player.en.pos.x as f32 - 1.,
+            player.en.pos.y as f32 - 1.,
             2.,
             2.,
             MAGENTA,
         );
         draw_text(
             &format!(
-                "Platformer mode: {} (tab)",
+                "Platformer mode: {} (tab) | solid collision: {} (c)",
                 if platformer_mode {
                     "space to jump"
                 } else {
                     "off"
+                },
+                if solid_collision {
+                    "on"
+                } else {
+                    "off (not implemented)"
                 }
             ),
             0.0,
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
 //! Simple 2d solid collision library.
 //!
-//! Based on https://maddythorson.medium.com/celeste-and-towerfall-physics-d24bd2ae0fc5
+//! Based on <https://maddythorson.medium.com/celeste-and-towerfall-physics-d24bd2ae0fc5>
+
+#![warn(missing_docs)]
 
 type Unit = i32;
 
+/// A 2 dimensional mathematical vector (x, y)
 #[derive(Clone, Copy, Debug)]
 pub struct Vec2 {
+    /// x component (horizontal)
     pub x: Unit,
+    /// y component (vertical)
     pub y: Unit,
 }
 
+/// Convenience function to create a Vec2 from (x, y)
 pub fn vec2(x: Unit, y: Unit) -> Vec2 {
     Vec2 { x, y }
 }
@@ -22,74 +28,112 @@ fn halfextent(v1: Unit, v2: Unit) -> Unit {
     (v1.max(v2) - v1.min(v2)).abs() / 2
 }
 
-pub fn pos_and_bb_centered(x1: Unit, y1: Unit, x2: Unit, y2: Unit) -> (Vec2, Vec2) {
-    let cx = center(x1, x2);
-    let cy = center(y1, y2);
-    let hhe = halfextent(x1, x2);
-    let vhe = halfextent(y1, y2);
-    (vec2(cx, cy), vec2(hhe, vhe))
-}
-
-pub fn pos_bb_xywh(pos: Vec2, bb: Vec2) -> (Unit, Unit, Unit, Unit) {
-    (pos.x - bb.x, pos.y - bb.y, bb.x * 2, bb.y * 2)
-}
-
-pub fn collision(en1: &Entity, en2: &Entity, en1_offset: Vec2) -> bool {
-    let x = en1.pos.x + en1_offset.x;
-    let y = en1.pos.y + en1_offset.y;
-    x + en1.bb.x > en2.pos.x - en2.bb.x
-        && x - en1.bb.x < en2.pos.x + en2.bb.x
-        && y + en1.bb.y > en2.pos.y - en2.bb.y
-        && y - en1.bb.y < en2.pos.y + en2.bb.y
-}
-
+/// 2D collision entity.
+///
+/// It has a 2d position, and a 2d bounding box that is centered on the position.
+#[derive(Clone, Copy, Debug)]
 pub struct Entity {
+    /// Position. This is the center point of the entity.
     pub pos: Vec2,
+    /// Bounding box, centered on `pos`,
     pub bb: Vec2,
 }
 
-pub fn move_x<F>(entity: &mut Entity, amount: f32, accum: &mut f32, mut collide_at: F) -> bool
-where
-    F: FnMut(&Entity, Vec2) -> bool,
-{
-    *accum += amount;
-    let mut move_amount = accum.round() as i32;
-    if move_amount != 0 {
-        *accum -= move_amount as f32;
-        let sign = move_amount.signum();
-        while move_amount != 0 {
-            if !collide_at(entity, vec2(sign, 0)) {
-                //There is no Solid immediately beside us
-                entity.pos.x += sign;
-                move_amount -= sign;
-            } else {
-                //Hit a solid!
-                return true;
-            }
+impl Entity {
+    /// Creates an entity from the 4 corners of a rectangle
+    pub fn from_rect_corners(x1: Unit, y1: Unit, x2: Unit, y2: Unit) -> Self {
+        let cx = center(x1, x2);
+        let cy = center(y1, y2);
+        let hhe = halfextent(x1, x2);
+        let vhe = halfextent(y1, y2);
+        Self {
+            pos: vec2(cx, cy),
+            bb: vec2(hhe, vhe),
         }
     }
-    false
+    /// Creates an entity from its component position and bounding box
+    pub fn from_pos_and_bb(pos: Vec2, bb: Vec2) -> Self {
+        Self { pos, bb }
+    }
+    /// Returns the (x, y, width, height) of the rectangle of this entity
+    pub fn xywh(&self) -> (Unit, Unit, Unit, Unit) {
+        (
+            self.pos.x - self.bb.x,
+            self.pos.y - self.bb.y,
+            self.bb.x * 2,
+            self.bb.y * 2,
+        )
+    }
+    /// Returns whether this entity would collide with another if its position was offset by `offset`
+    pub fn would_collide(&self, other: &Entity, offset: Vec2) -> bool {
+        let x = self.pos.x + offset.x;
+        let y = self.pos.y + offset.y;
+        x + self.bb.x > other.pos.x - other.bb.x
+            && x - self.bb.x < other.pos.x + other.bb.x
+            && y + self.bb.y > other.pos.y - other.bb.y
+            && y - self.bb.y < other.pos.y + other.bb.y
+    }
 }
 
-pub fn move_y<F>(entity: &mut Entity, amount: f32, accum: &mut f32, mut collide_at: F) -> bool
-where
-    F: FnMut(&Entity, Vec2) -> bool,
-{
-    *accum += amount;
-    let mut move_amount = accum.round() as i32;
-    if move_amount != 0 {
-        *accum -= move_amount as f32;
-        let sign = move_amount.signum();
-        while move_amount != 0 {
-            if !collide_at(entity, vec2(0, sign)) {
-                //There is no Solid immediately beside us
-                entity.pos.y += sign;
-                move_amount -= sign;
-            } else {
-                //Hit a solid!
-                return true;
-            }
+/// An [`Entity`] that can move.
+pub struct MobileEntity {
+    /// The [`Entity`] component of this `MobileEntity`
+    pub en: Entity,
+    accum: f32,
+}
+
+impl MobileEntity {
+    /// Creates a mobile entity from a position and bounding box
+    pub fn from_pos_and_bb(pos: Vec2, bb: Vec2) -> Self {
+        Self {
+            en: Entity::from_pos_and_bb(pos, bb),
+            accum: 0.0,
         }
     }
-    false
+    /// Move horizontally
+    pub fn move_x<F>(&mut self, amount: f32, mut collide_at: F) -> bool
+    where
+        F: FnMut(&Entity, Vec2) -> bool,
+    {
+        self.accum += amount;
+        let mut move_amount = self.accum.round() as i32;
+        if move_amount != 0 {
+            self.accum -= move_amount as f32;
+            let sign = move_amount.signum();
+            while move_amount != 0 {
+                if !collide_at(&self.en, vec2(sign, 0)) {
+                    //There is no Solid immediately beside us
+                    self.en.pos.x += sign;
+                    move_amount -= sign;
+                } else {
+                    //Hit a solid!
+                    return true;
+                }
+            }
+        }
+        false
+    }
+    /// Move vertically
+    pub fn move_y<F>(&mut self, amount: f32, mut collide_at: F) -> bool
+    where
+        F: FnMut(&Entity, Vec2) -> bool,
+    {
+        self.accum += amount;
+        let mut move_amount = self.accum.round() as i32;
+        if move_amount != 0 {
+            self.accum -= move_amount as f32;
+            let sign = move_amount.signum();
+            while move_amount != 0 {
+                if !collide_at(&self.en, vec2(0, sign)) {
+                    //There is no Solid immediately beside us
+                    self.en.pos.y += sign;
+                    move_amount -= sign;
+                } else {
+                    //Hit a solid!
+                    return true;
+                }
+            }
+        }
+        false
+    }
 }
