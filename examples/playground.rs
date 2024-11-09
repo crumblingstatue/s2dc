cc7a2c0724fb96ef4511c0fc6517bdf633d3ef5d
crumblingstatue <radiantstatue@gmail.com>
2024-11-09 17:55:23 +0100

    playground: Add ui for editing solids

--- examples/playground.rs
+++ examples/playground.rs
@@ -12,6 +12,7 @@ async fn main() {
     let gravity = 1.4;
     let mut platformer_mode = false;
     let mut solid_collision = true;
+    let mut solid_highlight_index = None;
     loop {
         clear_background(BLACK);
         if is_key_pressed(KeyCode::Tab) {
@@ -87,12 +88,15 @@ async fn main() {
             draw_rectangle(en.pos.x as f32, en.pos.y as f32, 1., 1., YELLOW);
         }
         let mut player_color = WHITE;
-        for solid in &solids {
+        for (i, solid) in solids.iter().enumerate() {
             let (x, y, w, h) = solid.xywh();
             let mut c2 = BROWN;
             if player.en.collides(solid) {
                 player_color = RED;
                 c2 = YELLOW;
+            }
+            if solid_highlight_index == Some(i) {
+                c2 = PURPLE;
             }
             draw_rectangle(x as f32, y as f32, w as f32, h as f32, c2);
         }
@@ -102,6 +106,39 @@ async fn main() {
                 ui.add(egui::DragValue::new(&mut player.en.pos.x).prefix("x "));
                 ui.add(egui::DragValue::new(&mut player.en.pos.y).prefix("y "));
                 ui.heading("Level");
+                let mut idx = 0;
+                let mut any_hov = false;
+                egui::Grid::new("solids_grid").show(ui, |ui| {
+                    ui.label("pos.x");
+                    ui.label("pos.y");
+                    ui.label("bb.x");
+                    ui.label("bb.y");
+                    ui.label("Delete");
+                    ui.end_row();
+                    solids.retain_mut(|solid| {
+                        let mut retain = true;
+                        let mut hov = false;
+                        hov |= ui.add(egui::DragValue::new(&mut solid.pos.x)).hovered();
+                        hov |= ui.add(egui::DragValue::new(&mut solid.pos.y)).hovered();
+                        hov |= ui.add(egui::DragValue::new(&mut solid.bb.x)).hovered();
+                        hov |= ui.add(egui::DragValue::new(&mut solid.bb.y)).hovered();
+                        let re = ui.button("x");
+                        hov |= re.hovered();
+                        if re.clicked() {
+                            retain = false;
+                        }
+                        if hov {
+                            solid_highlight_index = Some(idx);
+                            any_hov = true;
+                        }
+                        idx += 1;
+                        ui.end_row();
+                        retain
+                    });
+                });
+                if !any_hov {
+                    solid_highlight_index = None;
+                }
                 if ui.button("Clear").clicked() {
                     solids.clear();
                 }
